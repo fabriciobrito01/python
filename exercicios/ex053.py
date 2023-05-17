5f83a192f70d8b8655e9767c14fa6d5193a94300
fabriciobrito01 <fabriciobef@hotmail.com>
2023-05-17 17:05:12 -0300

    #

---
+++ exercicios/ex053.py
@@ -0,0 +1,4 @@
+frase = str(input('Digite uma frase para verificar se é um palídromo: ')).strip().upper()
+palavras = frase.split()
+junto = ''.join(palavras)
+...
--- exercicios/ex069.py
+++ exercicios/ex069.py
@@ -1,18 +1,18 @@
-maior = homem = mulher =  m20 = 0
-sexo = ' '
+maior = homem =  m20 = 0
 while True:
     idade = int(input('Idade: '))
+    sexo = ' '
+    while sexo not in 'MF':
+        sexo = str(input('Sexo [M/F]: ')).strip().upper()[0]
     if idade >= 18:
         maior += 1
-    while sexo not in 'MF':
-        sexo = str(input('Sexo [M/F]: ')).strip().upper()
-        if sexo == 'M':
-            sexo = homem
-            homem += 1
-        elif sexo == 'F' and idade < 20:
-            sexo = mulher
-            m20 += 1
-        esc = str(input('Deseja continuar [S/N]? ')).strip().upper()
-    if esc != 'S':
+    if sexo == 'M':
+        homem += 1
+    if sexo == 'F' and idade < 20:
+        m20 += 1
+    esc = ' '
+    while esc not in 'SN':
+        esc = str(input('Deseja continuar [S/N]? ')).strip().upper()[0]
+    if esc == 'N':
         break
 print(f'Foram cadastrados {maior} adultos, {homem} homens e {m20} mulheres abaixo dos 20 anos.')
